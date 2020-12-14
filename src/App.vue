841137df3bcec4223e80cf776360fed6a9799f7f
Hall <john.hall@name.com>
2020-12-14 12:08:37 -0700

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Reusable title="TOP"/>
+  <Reusable title="MIDDLE"/>
+  <Reusable title="BOTTOM"/>
+</template>
+
+<script>
+import Reusable from '@/components/Reusable.vue';
+
+export default {
+  components: {
+    Reusable,
+  },
+};
+</script>
---
+++ src/components/Reusable.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>This is the {{ title }} part of the page.</h1>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+  },
+};
+</script>
